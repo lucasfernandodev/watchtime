e85156fa68c704f5f50879520dabaebe13493a9c
Lucas Fernando <lucasfernando.dev@gmail.com>
2021-07-01 06:35:13 -0300

    feat: add função de pesquisar tempo por ip

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 <p align="center">
 <img src="./assets/img/screenshot.png" alt="watchtime screenshot">
 </p>
-<h1 align="center"><img src="./assets/img/logo.svg" width="22px" height:="22px"> watchtime</h1>
+<h1 align="center"><img src="./assets/img/logo.svg" width="22px" height:="22cdpx"> watchtime</h1>
 <p align="center">
-Um site simples desenvolvido para ver a previsão do tempo.
+Um website para ver a previsão do tempo.
 </p>
-<p align="center">Veja como ficou o site <a href="https://lucasfernandodev.github.io/watchtime/">clicando aqui</a>, a api usada é a <a href="https://console.hgbrasil.com/documentation/weather">HG Weather</a>.</p>
--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@
 const form = document.querySelector('.form');
 const main = document.querySelector('main')
 const sectionWeather = document.querySelector('.section-weather');
+const msg = document.querySelector('.msg');
+const searchIp = document.querySelector('.searchIp');
 const weather = document.querySelector('.weather-wrapper');
 const _close = weather.querySelector('.weather-wrapper__close')
 const weather__cityName = weather.querySelector('.weather-wrapper__city h3')
@@ -12,39 +14,56 @@ const weather__max = weather.querySelector('.weather-wrapper__minmax .max span')
 const weather__humidity = weather.querySelector('.weather-wrapper__umidade .value');
 const weather__data = weather.querySelector('.weather-wrapper__data .value');
 const weather__wind = weather.querySelector('.weather-wrapper__wind .value');
-
 const weather__weekday = weather.querySelectorAll('.weather-wrapper__dia');
 
 
-function closeSectionResultWeather(){
+function closeSectionResultWeather() {
     sectionWeather.style.display = 'none'
-    if(!main.classList.contains('loading')){
+    if (!main.classList.contains('loading')) {
         main.classList.add('loading')
+    }
+
+    if (sectionWeather.style.display == 'flex') {
+        msg.style.display = 'none'
+    } else {
+        msg.style.display = 'block'
     }
 }
 
-async function searchWeather(event) {
+async function searchWeather(event, userIP = false) {
     event.preventDefault();
-    if(!main.classList.contains('loading')){
+
+    let result;
+
+    if (!main.classList.contains('loading')) {
         main.classList.add('loading');
     }
 
     sectionWeather.style.display = 'flex';
 
+    if (sectionWeather.style.display == 'flex') {
+        msg.style.display = 'none'
+    } else {
+        msg.style.display = 'block'
+    }
+
     let city = form.querySelector('input').value;
 
     city = city.replace(' ', '_')
 
-
-    const result = await (await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=15486a1a&city_name=${city}`)).json();
-
-    if(result.error === true){
-        closeSectionResultWeather()
-        alert('Erro ao fazer busca')
-        return ;
+    if (userIP !== true) {
+         result = await (await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=15486a1a&city_name=${city}`)).json();
+    }else{
+         result = await (await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=15486a1a&user_ip=remote`)).json();
     }
 
-    const response =  result.results;
+    if (result.error === true && !result) {
+        closeSectionResultWeather()
+        alert('Erro ao fazer busca')
+        return;
+    }
+
+    const response = result.results;
 
 
     if (result !== 'undefined' || result.length > 0) {
@@ -79,11 +98,11 @@ async function searchWeather(event) {
             dayName.innerHTML = `${weekDay.weekday}`;
             min.innerHTML = `${weekDay.min}º`;
             max.innerHTML = `${weekDay.max}º`;
-          
+
         }
 
     }
-    else{
+    else {
         alert('erro ao fazer busca')
     }
 
@@ -93,3 +112,4 @@ async function searchWeather(event) {
 
 _close.addEventListener('click', closeSectionResultWeather);
 form.addEventListener('submit', event => searchWeather(event));
+searchIp.addEventListener('click', event => searchWeather(event, userIP = true))
