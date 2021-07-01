bd5bdcecf6831e75151628b051b55a6c5e6a40ac
Lucas Fernando <lucasfernando.dev@gmail.com>
2021-07-01 05:24:48 -0300

    feat: add msg de erro caso a cusca não funcione & add placeholder de loading & add title aos elementos html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,60 @@
 // Projeto weather
 
 const form = document.querySelector('.form');
-
+const main = document.querySelector('main')
+const sectionWeather = document.querySelector('.section-weather');
 const weather = document.querySelector('.weather-wrapper');
+const _close = weather.querySelector('.weather-wrapper__close')
 const weather__cityName = weather.querySelector('.weather-wrapper__city h3')
 const weather__temperatura = weather.querySelector('.weather-wrapper__temperatura h2')
 const weather__min = weather.querySelector('.weather-wrapper__minmax .min span')
 const weather__max = weather.querySelector('.weather-wrapper__minmax .max span')
 const weather__humidity = weather.querySelector('.weather-wrapper__umidade .value');
-const weather__sensation = weather.querySelector('.weather-wrapper__sensacao .value');
+const weather__data = weather.querySelector('.weather-wrapper__data .value');
 const weather__wind = weather.querySelector('.weather-wrapper__wind .value');
 
 const weather__weekday = weather.querySelectorAll('.weather-wrapper__dia');
 
+
+function closeSectionResultWeather(){
+    sectionWeather.style.display = 'none'
+    if(!main.classList.contains('loading')){
+        main.classList.add('loading')
+    }
+}
+
 async function searchWeather(event) {
     event.preventDefault();
+    if(!main.classList.contains('loading')){
+        main.classList.add('loading');
+    }
+
+    sectionWeather.style.display = 'flex';
 
     let city = form.querySelector('input').value;
 
     city = city.replace(' ', '_')
 
-    const result = await (await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=e35e4608&city_name=${city}`)).json();
-    const response = result.results;
 
-    console.log(response)
-    if (response) {
+    const result = await (await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=15486a1a&city_name=${city}`)).json();
+
+    if(result.error === true){
+        closeSectionResultWeather()
+        alert('Erro ao fazer busca')
+        return ;
+    }
+
+    const response =  result.results;
+
+
+    if (result !== 'undefined' || result.length > 0) {
+
+        main.classList.remove('loading');
 
         weather__cityName.innerHTML = response.city;
         weather__temperatura.innerHTML = `${response.temp}ºC ${response.description}`;
         weather__humidity.innerHTML = `${response.humidity}%`;
-        weather__sensation.innerHTML = `${response.date}`
+        weather__data.innerHTML = `${response.date}`
         weather__wind.innerHTML = `${response.wind_speedy}`
 
         const { forecast } = response;
@@ -57,9 +82,14 @@ async function searchWeather(event) {
           
         }
 
-        console.log(forecast)
+    }
+    else{
+        alert('erro ao fazer busca')
     }
 
 }
 
+
+
+_close.addEventListener('click', closeSectionResultWeather);
 form.addEventListener('submit', event => searchWeather(event));
